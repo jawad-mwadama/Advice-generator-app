82003be16986f6e553a2e80a0ad5e0ba5b409646
jawad-mwadama <allymwadama@gmail.com>
2025-10-02 23:22:32 +0300

    updated Ui colors

--- src/Components/AdviceCard.jsx
+++ src/Components/AdviceCard.jsx
@@ -40,13 +40,13 @@ function AdviceCard() {
       {error ? (
         <Error />
       ) : (
-        <main className="relative mx-3 max-w-sm rounded-2xl bg-slate-700 p-7 text-center sm:mx-4 sm:max-w-md md:mx-4.5">
-          <h5 className="font-light tracking-wider text-emerald-300">
+        <main className="relative mx-3 max-w-sm rounded-2xl bg-slate-600 p-7 text-center shadow-[0_10px_15px_-3px_hsl(218,23%,16%)] sm:mx-4 sm:max-w-md md:mx-4.5">
+          <h6 className="text-sm font-medium tracking-widest text-emerald-300 uppercase">
             Advice #{advice.id}
-          </h5>
+          </h6>
 
           <div className="">
-            <h6 className="py-3 text-2xl font-bold text-sky-200">
+            <h6 className="py-3 text-2xl font-bold text-sky-100">
               {isLoading ? <Loader /> : `"${advice?.advice}"`}
             </h6>
 
@@ -66,7 +66,7 @@ function AdviceCard() {
           </div>
 
           <div
-            className="absolute -bottom-5 left-1/2 mt-2 flex -translate-x-1/2 items-center justify-center rounded-full bg-emerald-300 sm:-bottom-6 md:-bottom-6"
+            className="absolute -bottom-5 left-1/2 mt-2 flex -translate-x-1/2 items-center justify-center rounded-full bg-emerald-300 transition-shadow duration-300 hover:bg-emerald-400 hover:shadow-[0_0_20px_4px_theme('colors.emerald.400')] sm:-bottom-6 md:-bottom-6"
             onClick={fetchAdvice}
             disabled={isLoading}
           >
